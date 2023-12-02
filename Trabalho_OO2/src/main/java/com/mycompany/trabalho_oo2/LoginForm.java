61154f39cae9355cd14dabf4a41848cd5471c828
Álvaro Davi Santos <104076110+dsalvaro@users.noreply.github.com>
2023-12-02 20:15:23 -0300

    criado LoginForm
    criada um teste de login pelo swing

---
+++ Trabalho_OO2/src/main/java/com/mycompany/trabalho_oo2/LoginForm.java
@@ -0,0 +1,48 @@
+package com.mycompany.trabalho_oo2;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginForm extends JFrame {
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+
+    public LoginForm(){
+        setTitle("Login Form");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 2));
+
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameField = new JTextField();
+
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordField = new JPasswordField();
+
+        JButton loginButton = new JButton("Login");
+
+        panel.add(usernameLabel);
+        panel.add(usernameField);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+        panel.add(new JLabel()); // espaço em branco
+        panel.add(loginButton);
+
+        add(panel);
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LoginForm();
+            }
+        });
+    }
+}
