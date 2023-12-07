47aaf29a638570d106e57372894a4e73eb9b12b3
Instituto de Ciencias Exatas <ice@lab06m41.nrc.ice.ufjf.br>
2023-12-06 22:44:25 -0300

    novas subclasses

---
+++ Trabalho_OO2/src/main/java/com/mycompany/trabalho_oo2/Meia.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trabalho_oo2;
+
+/**
+ *
+ * @author ice
+ */
+public class Meia {
+    
+}
