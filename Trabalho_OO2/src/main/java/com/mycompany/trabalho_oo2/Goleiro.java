ed0a4f721d1d5bb434f23cea44155fa897a2b047
Igor_Mattos_Motta <igormmattos06@gmail.com>
2023-12-10 16:55:44 -0300

    Adiciona master a main

---
+++ Trabalho_OO2/src/main/java/com/mycompany/trabalho_oo2/Goleiro.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trabalho_oo2;
+
+//Grupo:
+//Igor Mattos da Motta - 202276006
+//Álvaro Davi Carneiro dos Santos - 202176037
+//Davi Laranjo Villa 202235025
+public class Goleiro extends Jogador{
+    private int reflexo;
+    private int chute;
+    private int marcação;
+    private int passe;
+    private int velocidade;
+    private float notaGeral;
+
+    public Goleiro(CPF cpf, String nome, int cargo, String senha, String posicao, int numCamisa, Time time, boolean titular) {
+        super(cpf, nome, cargo, senha, posicao, numCamisa, time, titular);
+    }
+
+    public void setReflexo(int val)
+    {
+        this.reflexo = val;
+    }
+    
+    public int getReflexo()
+    {
+        return reflexo;
+    }
+    
+    public void setChute(int val)
+    {
+        this.chute = val;
+    }
+    
+    public int getChute()
+    {
+        return chute;
+    }
+    
+    public void setMarcação(int val)
+    {
+        this.marcação = val;
+    }
+    
+    public int getMarcação()
+    {
+        return marcação;
+    }
+    
+    public void setPasse(int val)
+    {
+        this.passe = val;
+    }
+    
+     public int getPasse()
+    {
+        return passe;
+    }
+     
+     public void setVelocidade(int val)
+    {
+        this.velocidade = val;
+    }
+     
+      public int getVelocidade()
+    {
+        return velocidade;
+    }
+      
+    /**
+     *
+     * @param jogador
+     * @return
+     */
+    @Override
+      public float calculaNotaGeral(Jogador jogador)
+      {
+          
+          this.notaGeral = (float) (reflexo*5+chute+marcação+passe*2+velocidade);
+          return notaGeral;
+      }
+}
