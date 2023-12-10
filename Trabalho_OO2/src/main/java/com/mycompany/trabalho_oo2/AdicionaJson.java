d38b4326d0a04e83932ad87a5b5cbb08ed9722b6
Álvaro Davi Santos <104076110+dsalvaro@users.noreply.github.com>
2023-12-10 15:32:36 -0300

    adicionaJson com as funções de adicionar jogador, técnico e time

--- Trabalho_OO2/src/main/java/com/mycompany/trabalho_oo2/AdicionaJson.java
+++ Trabalho_OO2/src/main/java/com/mycompany/trabalho_oo2/AdicionaJson.java
@@ -1,15 +1,12 @@
 package com.mycompany.trabalho_oo2;
 import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
-import java.io.FileReader;
-import java.io.FileWriter;
+
 public class AdicionaJson {
     private String nomeArquivo;
 
@@ -17,19 +14,134 @@ public class AdicionaJson {
         this.nomeArquivo = "C:\\Programas\\UFJF\\OO\\trabalho\\Trabalho_OO\\Trabalho_OO\\Trabalho_OO2\\src\\main\\java\\com\\mycompany\\trabalho_oo2\\data/";
     }
 
-    public void adicionaJogador(List<Jogador> jogadoresLista, List<Time> timesLista){
-        int idTime = 1;
+    public void adicionaJogador(
+            List<Jogador> jogadoresLista, List<Time> timesLista,
+            String cpf,String nome,int cargo,String senha,String posicao, int numCamisa,int idTime, boolean titular
+    ){
         Time timeJogador = null;
         for (Time time : timesLista){
             if (time.getId() == idTime){
                 timeJogador = time;
+                break;
             }
         }
-        Nome nome = new Nome("Bruno Guimaraes");
-        CPF cpf = new CPF("072.491.550-83");
-        Cargo cargo = new Cargo(3);
-        Jogador jogador = new Jogador(cpf, nome, cargo, "Guimaraesbrabo", "AT", 10, timeJogador, true);
-
+        Nome classNome = new Nome(nome);
+        CPF classCpf = new CPF(cpf);
+        Cargo classCargo = new Cargo(cargo);
+        Jogador jogador = new Jogador(classCpf, classNome, classCargo, senha, posicao, numCamisa, timeJogador, titular);
         jogadoresLista.add(jogador);
+
+        String nomeArquivo = this.nomeArquivo+"/jogadores.json";
+
+        try (FileReader fileReader = new FileReader(nomeArquivo)) {
+            // Ler o JSON existente
+            JsonElement jsonElement = JsonParser.parseReader(fileReader);
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+            // Obter a lista de jogadores
+            JsonArray jogadoresArray = jsonObject.getAsJsonArray("jogadores");
+
+            // Adicionar o novo jogador à lista
+            JsonObject novoJogador = new JsonObject();
+            novoJogador.addProperty("posicao", posicao);
+            novoJogador.addProperty("numCamisa", numCamisa);
+            novoJogador.addProperty("idTime", idTime);
+            novoJogador.addProperty("titular", titular);
+            novoJogador.addProperty("cpf", cpf);
+            novoJogador.addProperty("nome", nome);
+            novoJogador.addProperty("cargo", cargo);
+            novoJogador.addProperty("senha", senha);
+
+            jogadoresArray.add(novoJogador);
+
+            // Atualizar o JSON com a nova lista de jogadores
+            jsonObject.add("jogadores", jogadoresArray);
+
+            // Escrever o JSON atualizado de volta no arquivo
+            try (FileWriter fileWriter = new FileWriter(nomeArquivo)) {
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                gson.toJson(jsonObject, fileWriter);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void adicionaTecnico(
+            List<Tecnico> tecnicosLista,List<Time> timesLista,
+            String cpf,String nome,int cargo,String senha, int idTime
+    ){
+        Time timeTecnico = null;
+        for (Time time : timesLista){
+            if (time.getId() == idTime){
+                timeTecnico = time;
+                break;
+            }
+        }
+
+        Nome classNome = new Nome(nome);
+        CPF classCpf = new CPF(cpf);
+        Cargo classCargo = new Cargo(cargo);
+        Tecnico tecnico = new Tecnico(classCpf, classNome, classCargo, senha, timeTecnico);
+        tecnicosLista.add(tecnico);
+
+        String nomeArquivo = this.nomeArquivo+"/tecnicos.json";
+
+        try(FileReader fileReader = new FileReader(nomeArquivo)){
+            JsonElement jsonElement = JsonParser.parseReader(fileReader);
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+            JsonArray tecnicosArray = jsonObject.getAsJsonArray("tecnicos");
+
+            JsonObject novoTecnico = new JsonObject();
+            novoTecnico.addProperty("cpf", cpf);
+            novoTecnico.addProperty("nome", nome);
+            novoTecnico.addProperty("cargo", cargo);
+            novoTecnico.addProperty("senha", senha);
+            novoTecnico.addProperty("idTime", idTime);
+
+            tecnicosArray.add(novoTecnico);
+
+            jsonObject.add("tecnicos", tecnicosArray);
+
+            try(FileWriter fileWriter = new FileWriter(nomeArquivo)){
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                gson.toJson(jsonObject, fileWriter);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void adicionaTime(
+            List<Time> timesLista,
+            int id, String nome, String cidade
+    ){
+        String nomeArquivo = this.nomeArquivo+"/times.json";
+
+        try(FileReader fileReader = new FileReader(nomeArquivo)){
+            JsonElement jsonElement = JsonParser.parseReader(fileReader);
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+            JsonArray timesArray = jsonObject.getAsJsonArray("times");
+
+            JsonObject novoTime = new JsonObject();
+            novoTime.addProperty("id", id);
+            novoTime.addProperty("nome", nome);
+            novoTime.addProperty("cidade", cidade);
+
+            timesArray.add(novoTime);
+
+            jsonObject.add("times", timesArray);
+
+            try(FileWriter fileWriter = new FileWriter(nomeArquivo)){
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                gson.toJson(jsonObject, fileWriter);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Trabalho_OO2/src/main/java/com/mycompany/trabalho_oo2/Trabalho_OO2.java
+++ Trabalho_OO2/src/main/java/com/mycompany/trabalho_oo2/Trabalho_OO2.java
@@ -18,25 +18,23 @@ public class Trabalho_OO2 {
         leJson.getJogador(jogadoresLista,timesLista);
         leJson.getAdministrdor(adminLista);
         leJson.getGols(golsLista,jogadoresLista,partidasLista);
-        //AdicionaJson adicionaJson = new AdicionaJson();
-        //adicionaJson.adicionaJogador(jogadoresLista,timesLista);
 
         System.out.println("Tecnicos Cadastrados:");
         for(Tecnico tecnico : tecnicosLista){
             System.out.println(tecnico.getCpf());
+            System.out.println(tecnico.getCargo().getCargo());
         }
 
         System.out.println("Jogadores Cadastrados:");
         for(Jogador jogador : jogadoresLista){
             System.out.println(jogador.getCpf());
+            System.out.println(jogador.getCargo().getCargo());
         }
 
-        System.out.println("Times Cadastrados:");
-        for(Time time : timesLista){
-            System.out.println(time.getId());
-        }
-
-
+        AdicionaJson adicionaJson = new AdicionaJson();
+        //adicionaJson.adicionaJogador(jogadoresLista,timesLista,"693.241.520-84","Jogador Teste",1,"123456","Atacante",10,1,true);
+        //adicionaJson.adicionaTecnico(tecnicosLista,timesLista,"693.241.520-84","Tecnico Teste",1,"123456",1);
+        //adicionaJson.adicionaTime(timesLista,1,"Teste","Teste");
 
     }
 }
